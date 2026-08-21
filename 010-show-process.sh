9d60241e5f023156ba9269e00a36532d8ae1b615
IsaiasMN-AI <isaiasnunes680@gmail.com>
2026-08-21 10:10:08 -0400

    .

--- 010-show-process.sh
+++ 010-show-process.sh
@@ -6,12 +6,14 @@ read process
 # ps aux --> comando para puxar os processos
 # grep "$process" --> faz mostrar apenas as linha contém a palavra chave
 # grep -v "grep" --> ignora as linha que contém a palavra chave "grep"
-# wc -l --> transforma o output no número de linhas 
+# wc -l --> transforma o output no número de linhas
 running=$(ps aux | grep "$process" | grep -v "grep" | wc -l)
+process_list=$(ps aux | grep "$process" | grep -v "grep")
 
 if [ $? -eq 0 ]; then
     echo "The process $process is running."
     echo "Counter: $running"
+    echo "$process_list"
 else
     echo "There is no process named $process."
 fi
--- 012-filter-recursive.sh
+++ 012-filter-recursive.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 # Procura pela palavra ERROR em todos os diretórios e arquivos a partir do seu contexto.
-find_docker_logs=$(grep -r "ERROR" | grep -v "grep")
+find_docker_logs=$(grep -r "ERROR" . | grep -v "grep")
 
-echo $find_docker_logs
+echo "$find_docker_logs"
