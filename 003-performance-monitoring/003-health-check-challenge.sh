736c6d2b68eb8cea4a401d1c97adcc44e81265fb
IsaiasMN-AI <isaiasnunes680@gmail.com>
2026-08-31 12:20:01 -0400

    .

--- 003-performance-monitoring/003-health-check-challenge.sh
+++ 003-performance-monitoring/003-health-check-challenge.sh
@@ -4,7 +4,7 @@ while true; do
     load_average_1min=$(uptime | awk -F "load average:" '{print $2}' | awk '{print $1}' | tr -d ',.')
     if [ "$load_average_1min" -gt 100 ]; then
         notify-send -u critical -i warning "WARNING: CPU is overwhelmed!!!"
-        sleep 120
+        sleep 115
     fi
     sleep 5
 done
